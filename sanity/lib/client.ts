3ecd784741e439df6788d7dda7a03954e17515cb
vaibhavvsharma <35368146+vaibhavvsharma@users.noreply.github.com>
2023-11-18 17:26:20 +0530

    Sanity version upgraded..

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:
+    "sktb9IKLSPM5sfwqSGS76MAJsvlyWCpeaCjVmeeUi78V28WdqWHtHqKjXgn4ApDTycboHd0JsTlN5499DX7AwzlAanpdzo6I3nKdLnIH2vgw9QLEnEogGqc6rSYBUYWOitmpUue6iqWxVMS18rxa3k7ZZZfvD44VtFDZASYYTX7UXDSYXC1R",
 })
